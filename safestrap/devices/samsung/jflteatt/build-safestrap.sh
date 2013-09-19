4b66d7edf55c1415d174ed0ac78122106ecd224a
Hashcode <hashcode0f@gmail.com>
2013-09-19 13:47:26 -0700

    samsung/jflteatt: re-setup device support
    Change-Id: I4b10a9a5ef6b4d1298df609b0fd69ffa520cb453

---
+++ safestrap/devices/samsung/jflteatt/build-safestrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/build-safestrap.sh
+
---
+++ safestrap/devices/samsung/jflteatt/safestrap.mk
@@ -0,0 +1,4 @@
+include $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/safestrap-jf-common.mk
+
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
+

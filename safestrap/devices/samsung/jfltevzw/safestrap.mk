0db3a57500071bd8b91aa78d8e01dd11d4ea77c8
Hashcode <hashcode0f@gmail.com>
2013-09-19 13:47:42 -0700

    samsung/jfltevzw: re-setup device support
    Change-Id: I479f414af4dc86956f8076174384b10983d2708c

--- safestrap/devices/samsung/jfltevzw/safestrap.mk
+++ safestrap/devices/samsung/jfltevzw/safestrap.mk
@@ -1,37 +1,3 @@
-#TWRP
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-DEVICE_RESOLUTION := 1080x1920
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_GRAPHICS_DONT_DOUBLE_BUFFER := true
+include $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/safestrap-jf-common.mk
 
-RECOVERY_SDCARD_ON_DATA := true
-TW_INTERNAL_STORAGE_PATH := "/datamedia/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "datamedia"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-BOARD_HAS_NO_REAL_SDCARD := true
-
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_SAMSUNG := true
-TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic wait,check,encryptable=footer"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-
-SPLASH_RECOVERY_KEY := KEY_MENU
-SPLASH_CONTINUE_KEY := KEY_BACK
-
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TW_BRIGHTNESS_PATH := /sys/class/lcd/panel/backlight
-
-# Virtual partition size default (in mb)
-BOARD_DEFAULT_VIRT_SYSTEM_SIZE := 2800
-BOARD_DEFAULT_VIRT_CACHE_SIZE := 300
-
-HAVE_SELINUX := true
-

e04d4229230926c5406e2038bd0cd3f8446951e6
Hashcode <hashcode0f@gmail.com>
2013-09-19 13:45:05 -0700

    samsung/jf: commonize build-install.sh
    Change-Id: Ife39c21fe8402f7da1a8360da77ba51535935bdf

---
+++ safestrap/devices/common/build-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd $OUT
+rm $OUT/APP/install-files.zip
+rm $OUT/install-files/etc/safestrap/2nd-init.zip
+zip -9rj install-files/etc/safestrap/2nd-init 2nd-init-files/*
+cd $OUT/recovery/root
+# clean up for 2nd-init
+rm -rf data
+rm -rf dev
+rm -rf proc
+rm -rf sys
+rm -rf system
+rm -rf supersu
+rm -rf tmp
+
--- safestrap/devices/samsung/jflteatt-active/build-install.sh
+++ safestrap/devices/samsung/jflteatt-active/build-install.sh
@@ -2,8 +2,8 @@
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/build-install.sh
 cd $OUT/recovery/root
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/twrp.fstab ./etc/twrp.fstab
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/msm8960-common/init.rc ./init.rc
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung_s4-active/init.rc ./init.rc
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung_s4-active/twrp.fstab ./etc/twrp.fstab
 rm $OUT/install-files/etc/safestrap/ramdisk-recovery.img
 mkbootfs $OUT/recovery/root | minigzip > $OUT/install-files/etc/safestrap/ramdisk-recovery.img
 cd $OUT

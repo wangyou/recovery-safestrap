8ae00caed31bdde70daafbbb8cf9a4df21429752
Hashcode <hashcode0f@gmail.com>
2013-09-19 13:47:10 -0700

    samsung/jflteatt-active: re-setup device support
    Change-Id: I23bfb155482ef2a6b66baa5861bc767602d4c087

---
+++ safestrap/devices/samsung/jflteatt-active/safestrap.mk
@@ -0,0 +1,3 @@
+include $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/safestrap-jf-common.mk
+
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p27"
---
+++ safestrap/devices/samsung/jflteatt-active/sbin/build-fs.sh
@@ -0,0 +1,24 @@
+#!/sbin/bbx sh
+# By: Hashcode
+# Last Editted: 09/11/2013
+
+# system/userdata/cache
+IMAGE_NAME=`echo '${1}' | tr '[a-z]' '[A-Z]'`
+LOOP_DEV=${2}
+ROMSLOT_NAME=${3}
+
+BLOCK_DIR=/dev/block
+BLOCK_system=mmcblk0p16
+BLOCK_userdata=mmcblk0p27
+BLOCK_cache=mmcblk0p18
+BLOCK_boot=mmcblk0p20
+
+eval CURRENT_BLOCK=\$BLOCK_${IMAGE_NAME}
+
+SS_MNT=/ss
+SS_DIR=$SS_MNT/safestrap
+
+rm $BLOCK_DIR/$CURRENT_BLOCK
+ln -s $BLOCK_DIR/loop$LOOP_DEV $BLOCK_DIR/$CURRENT_BLOCK
+mke2fs -T ext4 $BLOCK_DIR/loop$LOOP_DEV
+

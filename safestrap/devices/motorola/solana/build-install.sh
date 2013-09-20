9113e3f83084bd6a3bc2e09ada4cb731a60dee4b
Hashcode <hashcode0f@gmail.com>
2013-09-19 21:38:41 -0700

    motorola/solana: update build
    Change-Id: Icc19b8192442be2ba6bf318e5f72d3f16e6c8280

--- safestrap/devices/motorola/solana/build-install.sh
+++ safestrap/devices/motorola/solana/build-install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/build-install.sh
+cd $OUT
+rm $OUT/APP/install-files.zip
+rm $OUT/install-files/etc/safestrap/2nd-init.zip
+rm $OUT/install-files/etc/safestrap/ramdisk-recovery.img
+zip -9rj install-files/etc/safestrap/2nd-init 2nd-init-files/*
+cd $OUT/recovery/root
+# we're using a real taskset binary
+rm -rf sbin/taskset
+cd $OUT/recovery/root
+# erase custom .rc files
+touch ./init.mapphone_cdma.rc
+touch ./init.mapphone_umts.rc
+# add common init.rc w/ battd
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/init.rc ./init.rc
 
 cd $OUT/recovery/root
 cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/targa/twrp.fstab ./etc/twrp.fstab

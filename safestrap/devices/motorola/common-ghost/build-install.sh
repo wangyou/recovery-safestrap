9935a5cef033862287ba6fb0a8bb3063a905c0cb
Hashcode <hashcode0f@gmail.com>
2013-09-19 21:37:53 -0700

    motorola/common-ghost: clear out more .rc files
    Change-Id: I050c66f33b3c10ccde85a79a30dffde85b5d75e4

--- safestrap/devices/motorola/common-ghost/build-install.sh
+++ safestrap/devices/motorola/common-ghost/build-install.sh
@@ -3,7 +3,10 @@ sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.s
 cd $OUT/recovery/root
 # erase custom .rc files
 touch ./init.mmi.usb.rc
+touch ./init.msm.rc
+touch ./init.product.rc
 touch ./init.qcom.rc
+touch ./init.sprint.rc
 touch ./init.target.rc
 touch ./init.vzw.rc
 # erase SElinux files

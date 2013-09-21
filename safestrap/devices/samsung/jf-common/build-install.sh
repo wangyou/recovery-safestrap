523760f8e7f124a009a8a320fd7652ee932ebfb3
Hashcode <hashcode0f@gmail.com>
2013-09-20 22:45:04 -0700

    samsung/jf: remove unneeded file overwrites now that hijack works better
    Change-Id: I4ecefc75b2e9dd1a96dd176d20a2d629a7735c8c

--- safestrap/devices/samsung/jf-common/build-install.sh
+++ safestrap/devices/samsung/jf-common/build-install.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
-cd $OUT/recovery/root
-# erase custom .rc files
-touch ./MSM8960_lpm.rc
-touch ./init.carrier.rc
-touch ./init.mmi.usb.rc
-touch ./init.qcom.rc
-touch ./init.qcom.usb.rc
-touch ./init.target.rc
-touch ./init.vzw.rc
-touch ./lpm.rc
-# erase SElinux files
-touch ./file_contexts
-touch ./property_contexts
-touch ./seapp_contexts
-touch ./sepolicy
-touch ./sepolicy_version
 

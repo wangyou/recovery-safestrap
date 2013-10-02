ba93c908faf01a616dc3724c408a155125780199
Hashcode <hashcode0f@gmail.com>
2013-10-02 01:22:26 -0700

    device/motorola/common-ghost: ss.config handling / global function changes
    Change-Id: Idc8228e5ad95a6273545378dcd7497549a84276b

--- safestrap/devices/motorola/common-ghost/build-install.sh
+++ safestrap/devices/motorola/common-ghost/build-install.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-ghost/init.rc ./init.rc
 cd $OUT/recovery/root
-# erase custom .rc files
-touch ./init.mmi.usb.rc
-touch ./init.msm.rc
-touch ./init.product.rc
-touch ./init.qcom.rc
-touch ./init.sprint.rc
-touch ./init.target.rc
-touch ./init.vzw.rc
-# erase SElinux files
-touch ./file_contexts
-touch ./property_contexts
-touch ./seapp_contexts
-touch ./sepolicy
-touch ./sepolicy_version
 
--- safestrap/devices/motorola/common-ghost/build-safestrap.sh
+++ safestrap/devices/motorola/common-ghost/build-safestrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-mkdir -p $OUT/recovery/root/sbin
 mkdir -p $OUT/APP
-mkdir -p $OUT/2nd-init-files
 mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 mkdir -p $OUT/install-files/etc/safestrap/rootfs/
+mkdir -p $OUT/recovery/root/etc/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola
-cp -fr common-ghost/APP/* $OUT/APP/
-cp -fr common-ghost/2nd-init-files/* $OUT/2nd-init-files/
 cp -fr common-ghost/res/* $OUT/install-files/etc/safestrap/res/
-cp -fr common-ghost/sbin/* $OUT/recovery/root/sbin/
 cp -fr common-ghost/hijack $OUT/install-files/bin/logwrapper
+cp -fr common-ghost/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
+cp -fr common-ghost/ss.config $OUT/install-files/etc/safestrap/ss.config
+cp -fr common-ghost/ss.config $OUT/APP/ss.config
+cp -fr common-ghost/ss.config $OUT/recovery/root/ss.config
 cd ../../../gui

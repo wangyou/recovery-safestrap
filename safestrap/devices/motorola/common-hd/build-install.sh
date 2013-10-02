b2f4c93fac5c3ec2b2ab1cd099f33cf5247b576e
Hashcode <hashcode0f@gmail.com>
2013-10-02 01:24:06 -0700

    device/motorola/common-hd: ss.config handling / global function changes
    Change-Id: Ia4b420e76c0b24dae84307c71a70b35acdf344e4

--- safestrap/devices/motorola/common-hd/build-install.sh
+++ safestrap/devices/motorola/common-hd/build-install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
 cd $OUT/recovery/root
-# erase custom .rc files
-touch ./init.mmi.usb.rc
-touch ./init.qcom.rc
-touch ./init.target.rc
-touch ./init.vzw.rc
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-hd/init.rc ./init.rc
 
--- safestrap/devices/motorola/common-hd/build-safestrap.sh
+++ safestrap/devices/motorola/common-hd/build-safestrap.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-mkdir -p $OUT/recovery/root/sbin
 mkdir -p $OUT/APP
-mkdir -p $OUT/2nd-init-files
 mkdir -p $OUT/install-files/etc/safestrap/res/
 mkdir -p $OUT/install-files/etc/safestrap/rootfs/
+mkdir -p $OUT/recovery/root/etc/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola
-cp -fr common-hd/APP/* $OUT/APP/
-cp -fr common-hd/2nd-init-files/* $OUT/2nd-init-files/
 cp -fr common-hd/res/* $OUT/install-files/etc/safestrap/res/
-cp -fr common-hd/sbin/* $OUT/recovery/root/sbin/
 cp -fr common-hd/hijack $OUT/install-files/etc/init.qcom.modem_links.sh
-cp -fr common-hd/init.rc $OUT/recovery/root/init.rc
+cp -fr common-hd/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
+cp -fr common-hd/ss.config $OUT/install-files/etc/safestrap/ss.config
+cp -fr common-hd/ss.config $OUT/APP/ss.config
+cp -fr common-hd/ss.config $OUT/recovery/root/ss.config
 cd ../../../gui

abfb04347b407c7bcf4611fa0755add494f63119
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:27:02 -0700

    motorola/solana: cleanup for ss.config changes
    Change-Id: Ie8e03c83929eeaa0d6f9fa5d7bd6516167fd18a0

--- safestrap/devices/motorola/solana/build-safestrap.sh
+++ safestrap/devices/motorola/solana/build-safestrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/build-safestrap.sh
 mkdir -p $OUT/recovery/root/sbin
 mkdir -p $OUT/APP
 mkdir -p $OUT/2nd-init-files
@@ -6,10 +7,12 @@ mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/kexec/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola
-cp -fr common-omap4/APP-1p21/* $OUT/APP/
-cp -fr solana/2nd-init-files/* $OUT/2nd-init-files/
 cp -fr common-omap4/res/* $OUT/install-files/etc/safestrap/res/
-cp -fr solana/sbin/* $OUT/recovery/root/sbin/
 cp -fr common-omap4/sbin-blobs/* $OUT/recovery/root/sbin/
 cp -fr solana/hijack $OUT/install-files/bin/logwrapper
+cp -fr solana/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
+cp -fr solana/ss.config $OUT/install-files/etc/safestrap/ss.config
+cp -fr solana/ss.config $OUT/APP/ss.config
+cp -fr solana/ss.config $OUT/recovery/root/ss.config
+cp -fr solana/safestrapmenu $OUT/install-files/etc/safestrap/safestrapmenu
 cd ../../../gui

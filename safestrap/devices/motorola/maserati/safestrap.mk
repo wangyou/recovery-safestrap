fdc8c94022b19bfbfc8e2e5c7ad8e07e46b94b27
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:25:59 -0700

    motorola/maserati: cleanup for ss.config changes
    Change-Id: I7cf40f1e58cad8749aa7d3e1f801f9321249f2d8

--- safestrap/devices/motorola/maserati/safestrap.mk
+++ safestrap/devices/motorola/maserati/safestrap.mk
@@ -2,4 +2,3 @@ include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestra
 include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/motorola/common-omap4/safestrap-common-omap4.mk
 
 TW_BRIGHTNESS_PATH := /sys/class/backlight/lm3532_bl/brightness
-

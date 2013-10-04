072b4a27eb97265eef3d04c3201ff45baf652526
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:23:24 -0700

    motorola/common-omap4: fix init.rc override
    Change-Id: I012120479a31bbd546167fd042cdc22484e76d2b

--- safestrap/devices/motorola/common-omap4/build-install.sh
+++ safestrap/devices/motorola/common-omap4/build-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/init.rc ./init.rc
 cd $OUT/recovery/root
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/init.rc ./init.rc
 

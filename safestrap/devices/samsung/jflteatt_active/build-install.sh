0e59f898fdab6b523c367c363051ca965535ccd5
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:22:32 -0700

    samsung/jflteatt_active: cleanup for ss.config changes
    Change-Id: Ib115189e43ef464377a8f964295bdb03ed1ab95c

--- safestrap/devices/samsung/jflteatt_active/build-install.sh
+++ safestrap/devices/samsung/jflteatt_active/build-install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/build-install.sh
-
-cd $OUT/recovery/root
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jflteatt_active/twrp.fstab ./etc/twrp.fstab
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/msm8960-common/init.rc ./init.rc
-
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
 

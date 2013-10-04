f48e6f1e84ccd1446d94b4e7efa0b42b5b292492
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:28:42 -0700

    motorola/targa: cleanup for ss.config changes
    Change-Id: I115ee7ffbb796ceaa53434b0c015021f862459c9

--- safestrap/devices/motorola/targa/build-safestrap.sh
+++ safestrap/devices/motorola/targa/build-safestrap.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p $OUT/recovery/root/sbin
-mkdir -p $OUT/APP
-mkdir -p $OUT/2nd-init-files
-mkdir -p $OUT/install-files/bin/
-mkdir -p $OUT/install-files/etc/safestrap/res/
-mkdir -p $OUT/install-files/etc/safestrap/rootfs/
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/build-safestrap.sh
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola
-cp -fr common-omap4/APP-1p21/* $OUT/APP/
-cp -fr targa/2nd-init-files/* $OUT/2nd-init-files/
-cp -fr common-omap4/res/* $OUT/install-files/etc/safestrap/res/
-cp -fr targa/sbin/* $OUT/recovery/root/sbin/
-cp -fr common-omap4/sbin-blobs/* $OUT/recovery/root/sbin/
-cp -fr targa/hijack $OUT/install-files/bin/logwrapper
-cd ../../../gui
+cp -fr targa/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
+cp -fr targa/ss.config $OUT/install-files/etc/safestrap/ss.config
+cp -fr targa/ss.config $OUT/APP/ss.config
+cp -fr targa/ss.config $OUT/recovery/root/ss.config

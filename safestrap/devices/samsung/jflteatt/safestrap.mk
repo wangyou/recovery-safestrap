bb466501fa3064ecedcde7a685b8eaec9c468cbd
Hashcode <hashcode0f@gmail.com>
2013-10-03 22:20:22 -0700

    samsung/jflteatt: cleanup for ss.config changes
    Change-Id: I0f5d4960ceb7c43a511e1d9c40c54fb86158918e

--- safestrap/devices/samsung/jflteatt/safestrap.mk
+++ safestrap/devices/samsung/jflteatt/safestrap.mk
@@ -2,4 +2,3 @@ include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestra
 include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/jf-common/safestrap-jf-common.mk
 
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
-

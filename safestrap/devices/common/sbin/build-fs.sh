db3e19db57634c655b2de74083c5279d53c050c9
Hashcode <hashcode0f@gmail.com>
2013-10-12 00:17:32 -0700

    bugfix: disable ext4 journaling if ss partitions are on emulated storage
    Change-Id: Iad0941a3a4fcd77a5f7f858b2b3c01245773624f

--- safestrap/devices/common/sbin/build-fs.sh
+++ safestrap/devices/common/sbin/build-fs.sh
@@ -8,6 +8,8 @@ LOOP_DEV=${2}
 ROMSLOT_NAME=${3}
 BBX=/sbin/bbx
 SS_CONFIG=/ss.config
+DISABLE_JOURNAL=
+
 
 . /sbin/ss_function.sh
 readConfig
@@ -16,13 +18,18 @@ $BBX eval CURRENT_BLOCK=\$BLOCK_${IMAGE_NAME}
 
 $BBX rm $BLOCK_DIR/$CURRENT_BLOCK
 $BBX ln -s $BLOCK_DIR/loop$LOOP_DEV $BLOCK_DIR/$CURRENT_BLOCK
+
+if [ "$SS_USE_DATAMEDIA" = "1" ]; then
+	DISABLE_JOURNAL="-O ^has_journal"
+fi
+
 if [ "$USERDATA_FSTYPE" = "f2fs" ] && [ "$LOOP_DEV" = "-userdata" ]; then
 	mkfs.f2fs $BLOCK_DIR/loop$LOOP_DEV
 else
 	if [ "$LOOP_DEV" = "-userdata" ]; then
-		mke2fs -T $USERDATA_FSTYPE $BLOCK_DIR/loop$LOOP_DEV
+		mke2fs $DISABLE_JOURNAL -T $USERDATA_FSTYPE $BLOCK_DIR/loop$LOOP_DEV
 	else
-		mke2fs -T $SYSTEM_FSTYPE $BLOCK_DIR/loop$LOOP_DEV
+		mke2fs $DISABLE_JOURNAL -T $SYSTEM_FSTYPE $BLOCK_DIR/loop$LOOP_DEV
 	fi
 fi
 

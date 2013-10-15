d9ec12817464664589abe1033a491ab95460a7c6
Hashcode <hashcode0f@gmail.com>
2013-10-14 22:17:19 -0700

    v3.63
    Change-Id: I72c40df287bae2be47b1e426b9f8e26c19b89b3a

--- variables.h
+++ variables.h
@@ -20,7 +20,7 @@
 #define TW_VERSION_STR              "2.6.3.0"
 
 #ifdef BUILD_SAFESTRAP
-#define SS_VERSION_STR              "3.62"
+#define SS_VERSION_STR              "3.63"
 #endif
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"

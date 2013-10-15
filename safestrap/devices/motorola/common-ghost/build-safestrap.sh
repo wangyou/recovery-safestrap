59b0e455e5020bb66143b430b9f17ec10d19deb5
Hashcode <hashcode0f@gmail.com>
2013-10-14 22:03:04 -0700

    motorola/common-ghost: change hijack to resize2fs and remove complexity
    Change-Id: Id9c8f1fb2fc2dd2e6791a1663ba76301fc570685

--- safestrap/devices/motorola/common-ghost/build-safestrap.sh
+++ safestrap/devices/motorola/common-ghost/build-safestrap.sh
@@ -3,14 +3,12 @@ mkdir -p $OUT/2nd-init-files/
 mkdir -p $OUT/APP
 mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
-mkdir -p $OUT/install-files/etc/safestrap/rootfs/
 mkdir -p $OUT/recovery/root/etc/
 mkdir -p $OUT/recovery/root/sbin/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola
 cp -fr common-ghost/APP/* $OUT/APP/
 cp -fr common-ghost/res/* $OUT/install-files/etc/safestrap/res/
-cp -fr common-ghost/sbin/* $OUT/recovery/root/sbin/
-cp -fr common-ghost/hijack $OUT/install-files/bin/hijack.sh
+cp -fr common-ghost/hijack $OUT/install-files/bin/resize2fs
 cp -fr common-ghost/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
 cp -fr common-ghost/ss.config $OUT/install-files/etc/safestrap/ss.config
 cp -fr common-ghost/ss.config $OUT/APP/ss.config

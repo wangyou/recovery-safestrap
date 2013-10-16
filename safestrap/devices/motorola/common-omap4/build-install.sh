2018a56d790605ed6f6df2738bfaa43f92d03cc0
Hashcode <hashcode0f@gmail.com>
2013-10-16 01:42:10 -0700

    motorola/common-omap4: make sure fixboot/bbx are correct versions
    Change-Id: I06005e0b1d6fd91fc4bd3a533ffc60452414b6a9

--- safestrap/devices/motorola/common-omap4/build-install.sh
+++ safestrap/devices/motorola/common-omap4/build-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
 cd $OUT/recovery/root
+# remove bbx and copy correct fixboot.sh
+rm ./sbin/bbx
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/2nd-init-files/fixboot.sh ./sbin/fixboot.sh
 cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/common-omap4/init.rc ./init.rc
 

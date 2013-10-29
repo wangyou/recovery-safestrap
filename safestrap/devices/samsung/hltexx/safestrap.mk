18947e57c8a352d7f7836689049c3bc30b9f487f
Hashcode <hashcode0f@gmail.com>
2013-10-29 15:22:41 -0700

    samsung/hltexx: initial note 3 device support (WIP)
    Change-Id: Ib8e9dfbb294dc06e7693d13d020894d20d8e1082

---
+++ safestrap/devices/samsung/hltexx/safestrap.mk
@@ -0,0 +1,6 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/h-common/safestrap-h-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true
+SS_TWRP_CALC_SMEM_LEN := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true

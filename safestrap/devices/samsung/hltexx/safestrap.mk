e22fba1bbe501a1042e4343ade4127a44cee96de
Hashcode <hashcode0f@gmail.com>
2013-10-31 11:26:27 -0700

    samsung/hlte-common: adjust build flags
    Change-Id: I9034ddbc01863687c49b98ccd921d55a62317e8d

--- safestrap/devices/samsung/hltexx/safestrap.mk
+++ safestrap/devices/samsung/hltexx/safestrap.mk
@@ -2,5 +2,3 @@ include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestra
 include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/h-common/safestrap-h-common.mk
 
 SS_TWRP_PRINT_SCREENINFO := true
-SS_TWRP_CALC_SMEM_LEN := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true

96c8ecde7f5dac3bd82db9ff19eebd5068b7a972
Hashcode <hashcode0f@gmail.com>
2013-12-11 20:40:03 -0800

    initial support for kfire hdx
    Change-Id: I759d3f969e218985ca2de473d36947238e06132f

---
+++ safestrap/devices/amazon/apollo/build-safestrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/amazon/common-hdx/build-safestrap.sh
+
+cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/amazon
+cp -fr apollo/res/* $OUT/install-files/etc/safestrap/res/
+
---
+++ safestrap/devices/amazon/common-hdx/build-safestrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+mkdir -p $OUT/recovery/root/etc
+mkdir -p $OUT/recovery/root/sbin
+mkdir -p $OUT/APP
+mkdir -p $OUT/install-files/etc/safestrap/res/
+mkdir -p $OUT/install-files/etc/safestrap/rootfs/
+cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/amazon
+cp -fr common-hdx/hijack $OUT/install-files/etc/resize_user_data.sh
+cp -fr common-hdx/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
+cp -fr common-hdx/ss.config $OUT/install-files/etc/safestrap/ss.config
+cp -fr common-hdx/ss.config $OUT/APP/ss.config
+cp -fr common-hdx/ss.config $OUT/recovery/root/ss.config
+cp -fr common-hdx/rootfs/* $OUT/install-files/etc/safestrap/rootfs/
+cp -fr ../../sbin-extras/* $OUT/recovery/root/sbin/
+cd ../../../gui

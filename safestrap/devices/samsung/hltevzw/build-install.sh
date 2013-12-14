a0f31eba9a649ad6fdebd9048e5baf77fed74d6f
Hashcode <hashcode0f@gmail.com>
2013-12-13 23:20:48 -0800

    hltevzw: add stock selinux files to build
    Change-Id: I2b638c6733b658f66b65398a32c2284ea3eab175

--- safestrap/devices/samsung/hltevzw/build-install.sh
+++ safestrap/devices/samsung/hltevzw/build-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/h-common/build-install.sh
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/hltevzw/selinux/* $OUT/recovery/root/
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
 

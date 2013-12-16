a661beb2c75180a29dcaa3826263bb797c6631dc
Hashcode <hashcode0f@gmail.com>
2013-12-16 15:03:54 -0800

    jf-common: update hijack for 4.3 SElinux
    Change-Id: Id758d23b06cdff7df4861b2045c085d4d84dff11

--- safestrap/devices/samsung/jf-common/build-safestrap.sh
+++ safestrap/devices/samsung/jf-common/build-safestrap.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p $OUT/recovery/root/etc
 mkdir -p $OUT/recovery/root/sbin
-mkdir -p $OUT/APP
+mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 mkdir -p $OUT/install-files/etc/safestrap/rootfs/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
-cp -fr jf-common/APP/* $OUT/APP/
 cp -fr jf-common/res/* $OUT/install-files/etc/safestrap/res/
 cp -fr jf-common/sbin-libs/* $OUT/recovery/root/sbin/
-cp -fr jf-common/hijack $OUT/install-files/etc/init.qcom.modem_links.sh
+cp -fr jf-common/hijack $OUT/install-files/bin/e2fsck
 cp -fr jf-common/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
 cp -fr jf-common/ss.config $OUT/install-files/etc/safestrap/ss.config
 cp -fr jf-common/ss.config $OUT/APP/ss.config

8a899c825863ab73e505d1b6408eb6a3af493a84
Hashcode <hashcode0f@gmail.com>
2013-12-16 15:43:05 -0800

    initial hlteatt support
    Change-Id: I2779b57b6934b4118ca77a89165d0292612873c5

---
+++ safestrap/devices/samsung/hlteatt/build-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/h-common/build-install.sh
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/hlteatt/selinux/* $OUT/recovery/root/
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
+
---
+++ safestrap/devices/samsung/hlteatt/safestrap.mk
@@ -0,0 +1,4 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/h-common/safestrap-h-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true

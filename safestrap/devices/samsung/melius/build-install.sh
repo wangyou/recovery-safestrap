1090350047bfc0df4941a39e552d5e42d449f2b3
Hashcode <hashcode0f@gmail.com>
2013-12-18 23:48:48 -0800

    samsung/melius: updates
    Change-Id: Ic1280f9eb348dbf338164de43fd953baf63753e0

--- safestrap/devices/samsung/melius/build-install.sh
+++ safestrap/devices/samsung/melius/build-install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install.sh
 cd $OUT/recovery/root
+# remove fstab.qcom from recovery
+rm fstab.qcom
 cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/melius/init.rc ./init.rc
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
 

c74ccfb756cbbdcbf033224338e778be351822a4
Hashcode <hashcode0f@gmail.com>
2014-01-11 19:54:39 -0800

    samsung/hltexx: fix twrp.fstab and system size
    Change-Id: I2753696121e37f6b19ed0044d20bf75bc2260043

--- safestrap/devices/samsung/hltexx/build-safestrap.sh
+++ safestrap/devices/samsung/hltexx/build-safestrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/h-common/build-safestrap.sh
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
+cp -fr hltexx/twrp.fstab $OUT/recovery/root/etc/twrp.fstab
 cp -fr hltexx/ss.config $OUT/install-files/etc/safestrap/ss.config
 cp -fr hltexx/ss.config $OUT/APP/ss.config
 cp -fr hltexx/ss.config $OUT/recovery/root/ss.config

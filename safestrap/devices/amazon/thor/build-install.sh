3b4af311102a9ca2736e8fdf200b1663747f954c
Hashcode <hashcode0f@gmail.com>
2014-01-15 23:57:00 -0800

    amazon/thor: setup unique init.rc
    Change-Id: I40996637042f2b27fddae5fa7f7a65df54108951

--- safestrap/devices/amazon/thor/build-install.sh
+++ safestrap/devices/amazon/thor/build-install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/amazon/common-hdx/build-install.sh
+cd $OUT/recovery/root
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/amazon/thor/init.rc ./init.rc
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
 

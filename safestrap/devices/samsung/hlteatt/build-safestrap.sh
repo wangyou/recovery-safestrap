6924321bc52a16f02960e3767157a13f626f32cf
Hashcode <hashcode0f@gmail.com>
2014-04-02 00:02:42 -0700

    samsung/hlteatt: move custom system image creation to common
    Change-Id: I43f99b197a734c50b485efae067e56b86ca3ac47

--- safestrap/devices/samsung/hlteatt/build-safestrap.sh
+++ safestrap/devices/samsung/hlteatt/build-safestrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/h-common/build-safestrap.sh
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
-cp -fr hlteatt/build-fs.sh $OUT/recovery/root/sbin/
 

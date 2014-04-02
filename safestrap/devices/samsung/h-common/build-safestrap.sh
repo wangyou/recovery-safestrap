6fc5b13ca7252c9341d527bf6e68e87076b3e410
Hashcode <hashcode0f@gmail.com>
2014-04-02 00:01:03 -0700

    samsung/hlte-common: custom APP scripts and system image build
    Change-Id: I2f7cf73494215eb8ee2801cda27acd4dc778853c

--- safestrap/devices/samsung/h-common/build-safestrap.sh
+++ safestrap/devices/samsung/h-common/build-safestrap.sh
@@ -5,6 +5,7 @@ mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 mkdir -p $OUT/install-files/etc/safestrap/rootfs/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
+cp -fr h-common/APP/* $OUT/APP/
 cp -fr h-common/res/* $OUT/install-files/etc/safestrap/res/
 cp -fr h-common/sbin-libs/* $OUT/recovery/root/sbin/
 cp -fr h-common/hijack $OUT/install-files/bin/e2fsck
@@ -13,5 +14,6 @@ cp -fr h-common/ss.config $OUT/install-files/etc/safestrap/ss.config
 cp -fr h-common/ss.config $OUT/APP/ss.config
 cp -fr h-common/ss.config $OUT/recovery/root/ss.config
 cp -fr h-common/rootfs/* $OUT/install-files/etc/safestrap/rootfs/
+cp -fr h-common/build-fs.sh $OUT/recovery/root/sbin/
 cp -fr ../../sbin-extras/* $OUT/recovery/root/sbin/
 cd ../../../gui

f4565b14777f1ac794d366f8a8ec618a6ff2b220
Hashcode <hashcode0f@gmail.com>
2014-06-24 10:39:34 -0700

    add initial kltevzw support
    Change-Id: I4fbba3d790e4f50f2fc0f43fe6e551e751a1409e

---
+++ safestrap/devices/samsung/kltevzw/safestrap.mk
@@ -0,0 +1,4 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/k-common/safestrap-k-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true

f64aef126c73283dc8acbd9feab797d281b7ebef
Hashcode <hashcode0f@gmail.com>
2014-06-24 23:02:28 -0700

    kltevzw: add vzw s5 hacked init binary
    Change-Id: If4c32a979fbdd9eb8109065d068ef7081ea1d46b

--- safestrap/devices/samsung/kltevzw/build-safestrap.sh
+++ safestrap/devices/samsung/kltevzw/build-safestrap.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/k-common/build-safestrap.sh
+cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
+cp -fr kltevzw/rootfs/* $OUT/install-files/etc/safestrap/rootfs/
+

f57eb1bc40cc4a9eb60fe36393f94cce3ada5faa
Hashcode <hashcode0f@gmail.com>
2014-06-26 14:24:53 -0700

    jfltevzw: remove static selinux files
    Change-Id: I495769896b0b5a4a2a6c536a7aa8dedbc0b5d83d

--- safestrap/devices/samsung/jfltevzw/build-install.sh
+++ safestrap/devices/samsung/jfltevzw/build-install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jf-common/build-install.sh
-cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/jfltevzw/selinux/* $OUT/recovery/root/
 sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/build-install-finish.sh
 

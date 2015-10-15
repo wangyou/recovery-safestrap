b9ac679642aae6390dab9d4ed6dd2bea8ac2459d
mohammad <mohammad@mohammad.afaneh>
2015-10-15 22:04:22 +0300

    Fix up some TW_OEM_BUILD things Fix factory reset now properly formats data as expected Change text for ORS processing Disable system read only check, SuperSU, and patch system Stop copying fstab and version to cache

--- twrp.cpp
+++ twrp.cpp
@@ -358,6 +358,7 @@ int main(int argc, char **argv) {
 	// Launch the main GUI
 	gui_start();
 
+#ifndef TW_OEM_BUILD
 	// Disable flashing of stock recovery
 	TWFunc::Disable_Stock_Recovery_Replace();
 	// Check for su to see if the device is rooted or not
@@ -372,6 +373,7 @@ int main(int argc, char **argv) {
 		sync();
 		PartitionManager.UnMount_By_Path("/system", false);
 	}
+#endif
 
 	// Reboot
 	TWFunc::Update_Intent_File(Reboot_Value);

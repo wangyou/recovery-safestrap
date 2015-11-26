d756a92cbd656bac7d86a3b09546ae7834007fe0
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2015-11-26 19:28:42 +0200

    t0ltevzw: add custom APP scripts

--- safestrap/devices/samsung/t0-common/build-safestrap.sh
+++ safestrap/devices/samsung/t0-common/build-safestrap.sh
@@ -5,6 +5,7 @@ mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 mkdir -p $OUT/install-files/etc/safestrap/rootfs/
 cd $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung
+cp -fr t0-common/APP/* $OUT/APP/
 cp -fr t0-common/res/* $OUT/install-files/etc/safestrap/res/
 cp -fr t0-common/hijack $OUT/install-files/bin/e2fsck
 cp -fr t0-common/twrp.fstab $OUT/recovery/root/etc/twrp.fstab

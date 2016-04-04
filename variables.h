a8dd05a740618453aabca1bac9307b4f15099291
Ethan Yonker <dees_troy@teamw.in>
2016-04-04 09:25:59 -0500

    3.0.2-0
    Change-Id: I28fa04cb90dd4255db9dc5d771416ab8d456de46

--- variables.h
+++ variables.h
@@ -17,7 +17,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define TW_VERSION_STR              "3.0.1-0"
+#define TW_VERSION_STR              "3.0.2-0"
 
 #ifdef BUILD_SAFESTRAP
 #define SS_VERSION_STR              "4.02"

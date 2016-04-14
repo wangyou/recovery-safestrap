9ce584cbc47e38a3e744734328549e93a9dbdd40
mohammad <mohammad@mohammad.afaneh>
2016-04-14 19:13:18 +0300

    version 4.03

--- variables.h
+++ variables.h
@@ -20,7 +20,7 @@
 #define TW_VERSION_STR              "3.0.2-0"
 
 #ifdef BUILD_SAFESTRAP
-#define SS_VERSION_STR              "4.02"
+#define SS_VERSION_STR              "4.03"
 #endif
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
